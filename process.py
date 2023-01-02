9792513ae789d4c2f63fdafdaefedd9fc2648d1e
tbellicha <tanguy.bellicha@epitech.eu>
2023-01-02 02:22:08 +0100

    [ADD] Default values from JSON and the game data

--- process.py
+++ process.py
@@ -2,6 +2,55 @@
 import json
 import sys
 
+from values import *
+
+class Rune:
+    def __init__(self, rune):
+        self.rune_id = rune["rune_id"]
+        self.equipped = rune["occupied_type"]
+        self.slot = rune["slot_no"]
+        self.quality = rune["rank"]
+        self.nb_stars = rune["class"]
+        self.set = rune["set_id"]
+        self.main_stat_id = rune["pri_eff"][0]
+        self.innate_stat_id = rune["prefix_eff"][0]
+        self.innate_stat_value = rune["prefix_eff"][1]
+        self.substats = []
+        for substat in rune["sec_eff"]:
+            self.substats.append(substat)
+        self.efficiency = 0
+
+
+def calc_efficiency(rune):
+    if rune.nb_stars == Stars.SIX.value or Stars.SIX_ANTIC.value:
+        eff_main = 1
+        eff_innate = 0
+        eff_subs = 0
+        if rune.innate_stat_id != Stat.NONE.value:
+            eff_innate = rune.innate_stat_value / (MaxValueStat6[Stat(rune.innate_stat_id).name].value * 5)
+        for substat in rune.substats:
+            if substat[0] != Stat.NONE.value:
+                eff_subs += (substat[1] + substat[3]) / (MaxValueStat6[Stat(substat[0]).name].value * 5)
+        rune.efficiency = ((eff_main + eff_innate + eff_subs) / 2.8) * 100
+        return
+    if rune.nb_stars == Stars.FIVE.value or Stars.FIVE_ANTIC.value:
+        eff_main = 1 # to change
+        eff_innate = 0
+        eff_subs = 0
+        if rune.innate_stat_id != Stat.NONE.value:
+            eff_innate = rune.innate_stat_value / (MaxValueStat5[Stat(rune.innate_stat_id).name].value * 5)
+        for substat in rune.substats:
+            if substat[0] != Stat.NONE.value:
+                eff_subs += (substat[1] + substat[3]) / (MaxValueStat5[Stat(substat[0]).name].value * 5)
+        rune.efficiency = ((eff_main + eff_innate + eff_subs) / 2.8) * 100
+        return
+
+
+def compute_rune(rune):
+    crune = Rune(rune)
+    calc_efficiency(crune)
+    return crune
+
 
 def process(argv):
     try:
@@ -10,5 +59,23 @@ def process(argv):
         print("Cannot open file", file=sys.stderr)
         return 84
     data = json.load(file)
+    name = data["wizard_info"]["wizard_name"]
+    all_account_runes = []
+    print(name)
+    i = 0
+    #  Runes from monsters
+    for monster in data["unit_list"]:
+        for rune in monster["runes"]:
+            crune = compute_rune(rune)
+            if crune.efficiency >= 100:
+                i += 1
+            all_account_runes.append(crune)
+    #  Runes from inventory
+    for rune in data["runes"]:
+        crune = compute_rune(rune)
+        if crune.efficiency >= 100:
+            i += 1
+        all_account_runes.append(crune)
+    print(i)
     file.close()
     return 0
---
+++ values.py
@@ -0,0 +1,103 @@
+from enum import Enum
+
+
+#  set_id
+class Set(Enum):
+    ENERGY          = 1
+    GUARD           = 2
+    SWIFT           = 3
+    BLADE           = 4
+    RAGE            = 5
+    FOCUS           = 6
+    ENDURE          = 7
+    FATAL           = 8
+    DESPAIR         = 10
+    VAMPIRE         = 11
+    VIOLENT         = 13
+    NEMESIS         = 14
+    WILL            = 15
+    SHIELD          = 16
+    REVENGE         = 17
+    DESTROY         = 18
+    FIGHT           = 19
+    DETERMINATION   = 20
+    ENHANCE         = 21
+    ACCURACY        = 22
+    TOLERANCE       = 23
+
+
+#  first value of prefix_eff/sec_eff
+class Stat(Enum):
+    NONE        = 0
+    HP          = 1
+    HP_PERCENT  = 2
+    ATK         = 3
+    ATK_PERCENT = 4
+    DEF         = 5
+    DEF_PERCENT = 6
+    SPD         = 8
+    CRIT_RATE   = 9
+    CRIT_DMG    = 10
+    RES         = 11
+    ACC         = 12
+
+
+#  rank
+class Quality(Enum):
+    NORMAL          = 1
+    MAGIC           = 2
+    RARE            = 3
+    HERO            = 4
+    LEGEND          = 5
+    NORMAL_ANTIC    = 11
+    MAGIC_ANTIC     = 12
+    RARE_ANTIC      = 13
+    HERO_ANTIC      = 14
+    LEGEND_ANTIC    = 15
+
+
+#  class
+class Stars(Enum):
+    ONE         = 1
+    TWO         = 2
+    THREE       = 3
+    FOUR        = 4
+    FIVE        = 5
+    SIX         = 6
+    ONE_ANTIC   = 11
+    TWO_ANTIC   = 12
+    THREE_ANTIC = 13
+    FOUR_ANTIC  = 14
+    FIVE_ANTIC  = 15
+    SIX_ANTIC   = 16
+
+
+coeff_flat = 0.5
+
+
+class MaxValueStat6(Enum):
+    HP          = 375 / coeff_flat
+    HP_PERCENT  = 8
+    ATK         = 20  / coeff_flat
+    ATK_PERCENT = 8
+    DEF         = 20  / coeff_flat
+    DEF_PERCENT = 8
+    SPD         = 6
+    CRIT_RATE   = 6
+    CRIT_DMG    = 7
+    RES         = 8
+    ACC         = 8
+
+
+class MaxValueStat5(Enum):
+    HP          = 300 / coeff_flat
+    HP_PERCENT  = 7
+    ATK         = 15  / coeff_flat
+    ATK_PERCENT = 7
+    DEF         = 15  / coeff_flat
+    DEF_PERCENT = 7
+    SPD         = 5
+    CRIT_RATE   = 5
+    CRIT_DMG    = 5
+    RES         = 7
+    ACC         = 7
