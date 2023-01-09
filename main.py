af3268ca10042c4cc29dca1e24ec8db16664fb63
tbellicha <tanguy.bellicha@epitech.eu>
2023-01-09 11:05:10 +0100

    [ADD] Handle flags

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def print_helper():
 def check_args(argc, argv):
     if argc == 2 and argv[1] == "-h":
         return print_helper()
-    if argc != 2:
+    if not 2 <= argc <= 3:
         print("Bad arguments number, try with -h", file=sys.stderr)
         return -1
     return 0
@@ -27,5 +27,12 @@ def main(argc, argv):
         return 84
     elif args_return == 1:
         return 0
-    else:
-        return process.process(argv)
+    if argc == 3 and argv[2] == '-g':
+        return process.process_grind(argv)
+    if argc == 3 and argv[2] == '-e':
+        return process.process_enchant(argv)
+    if argc == 3 and argv[2] == '-s':
+        return process.process_score(argv)
+    if argc == 3 and argv[2] == '-u':
+        return process.process_update(argv)
+    return process.process_grind(argv)
